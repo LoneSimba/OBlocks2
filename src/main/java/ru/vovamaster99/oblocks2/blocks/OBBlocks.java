003d5207a250e49021f9d17ee133b648c1965e96
VovaMaster <vovamaster99@gmail.com>
2014-12-30 23:13:10 +0400

    Final update #1
    Финальная сборка 1. Добавлены почти все блоки.
    Final build 1. Added almost all blocks.

--- src/main/java/ru/vovamaster99/oblocks2/blocks/OBBlocks.java
+++ src/main/java/ru/vovamaster99/oblocks2/blocks/OBBlocks.java
@@ -16,16 +16,31 @@ public class OBBlocks {
 	
 
 
-	public static Block lCobblestone;	
+	public static Block lCobblestone;
+	public static Block lStone;	
 	public static Block lGlowstone;
-	public static Block airCobblestone;
-	public static Block airStone;
-	public static Block lStone;
-	public static Block airGlowstone;
+	
+	public static Block fakeCobblestone;
+	public static Block fakeStone;
+	public static Block fakeGlowstone;
+	public static Block fakeStoneBrick;
+	public static Block fakeBrokenStoneBrick;
+	public static Block fakeMossyStoneBrick;
+	public static Block fakeFancyStoneBrick;
+	public static Block fakeBrick;
+	public static Block fakeGlass;
+	public static Block fakeBrichPlanks;
+	public static Block fakeJunglePlanks;
+	public static Block fakeSprucePlanks;
+	public static Block fakeOakPlanks;
+	public static Block fakeAcaciaPlanks;
+	public static Block fakeDarkOakPlanks;
+	
 	public static Block crystalLatern;
-	public static Block airOre;
-	public static Block crystalOre;
 	public static Block plastic;
+	public static Block airOre;
+	
+	public static Block crystalOre;
 	public static Block matterOre;
 	public static Block polymerOre;
     public static Block rubyOre;
@@ -50,9 +65,21 @@ public class OBBlocks {
 		lGlowstone = new LightBlock().setBlockName("lGlowstone");
 		lStone = new LightBlock().setBlockName("lStone");
 		
-		airCobblestone = new AirBlock(Material.ground).setBlockName("airCobblestone").setBlockTextureName("oblocks2:airCobblestone");
-		airStone = new AirBlock(Material.rock).setBlockName("airStone").setBlockTextureName("oblocks2:airStone");
-	    airGlowstone = new AirBlock(Material.glass).setBlockName("airGlowstone").setBlockTextureName("oblocks2:airGlowstone").setLightLevel(1.0F);
+		fakeCobblestone = new FakeBlock(Material.rock).setBlockName("fakeCobblestone").setBlockTextureName("oblocks2:fakeCobblestone").setHardness(1.5F).setResistance(10.0F);
+		fakeStone = new FakeBlock(Material.rock).setBlockName("fakeStone").setBlockTextureName("oblocks2:fakeStone").setHardness(2.0F).setResistance(10.0F);
+	    fakeGlowstone = new FakeBlock(Material.glass).setBlockName("fakeGlowstone").setBlockTextureName("oblocks2:fakeGlowstone").setLightLevel(1.0F).setStepSound(Block.soundTypeGlass).setHardness(0.2F).setResistance(0.5F);
+	    fakeStoneBrick = new FakeBlock(Material.rock).setBlockName("fakeStoneBrick").setBlockTextureName("oblocks2:fakeStoneBrick").setHardness(2.0F).setResistance(10.0F);
+	    fakeBrokenStoneBrick = new FakeBlock(Material.rock).setBlockName("fakeBrokenStoneBrick").setBlockTextureName("oblocks2:fakeBrokenStoneBrick").setHardness(1.5F).setResistance(7.0F);
+	    fakeMossyStoneBrick = new FakeBlock(Material.rock).setBlockName("fakeMossyStoneBrick").setBlockTextureName("oblocks2:fakeMossyStoneBrick").setHardness(1.5F).setResistance(7.0F);
+	    fakeFancyStoneBrick = new FakeBlock(Material.rock).setBlockName("fakeFancyStoneBrick").setBlockTextureName("oblocks2:fakeFancyStoneBrick").setHardness(1.5F).setResistance(7.0F);
+        fakeBrichPlanks = new FakeBlock(Material.wood).setBlockName("fakeBrichPlanks").setBlockTextureName("oblocks2:fakeBrichPlanks").setHardness(1.0F).setResistance(6.0F);
+        fakeJunglePlanks = new FakeBlock(Material.wood).setBlockName("fakeJunglePlanks").setBlockTextureName("oblocks2:fakeJunglePlanks").setHardness(1.0F).setResistance(6.0F);
+        fakeSprucePlanks = new FakeBlock(Material.wood).setBlockName("fakeSprucePlanks").setBlockTextureName("oblocks2:fakeSprucePlanks").setHardness(1.0F).setResistance(6.0F);
+        fakeOakPlanks = new FakeBlock(Material.wood).setBlockName("fakeOakPlanks").setBlockTextureName("oblocks2:fakeOakPlanks").setHardness(1.0F).setResistance(6.0F);
+        fakeAcaciaPlanks = new FakeBlock(Material.wood).setBlockName("fakeAcaciaPlanks").setBlockTextureName("oblocks2:fakeAcaciaPlanks").setHardness(1.0F).setResistance(6.0F);
+        fakeDarkOakPlanks = new FakeBlock(Material.wood).setBlockName("fakeDarkOakPlanks").setBlockTextureName("oblocks2:fakeDarkOakPlanks").setHardness(1.0F).setResistance(6.0F);
+	    fakeBrick = new FakeBlock(Material.rock).setBlockName("fakeBrick").setBlockTextureName("oblocks2:fakeBrick").setHardness(2.0F).setResistance(7.0F);
+	    fakeGlass = new FakeBlock(Material.glass).setBlockName("fakeGlass").setBlockTextureName("oblocks2:fakeGlass").setStepSound(Block.soundTypeGlass).setHardness(0.2F).setResistance(0.5F);
 	    
 	    crystalLatern = new BlockModel(Material.glass).setBlockName("crystalLatern");
 	    plastic = new BlockModel(Material.ground).setBlockName("plastic");
@@ -65,13 +92,25 @@ public class OBBlocks {
 	    ironSlabSingle = (BlockSlab) new IronSlabBlock(false, ironSlabSingle);
 	    ironSlabDouble = (BlockSlab) new IronSlabBlock(true, ironSlabDouble);
 	    
-	    GameRegistry.registerBlock(lCobblestone, "lCobblestone");
-	    GameRegistry.registerBlock(lStone, "lStone");
-	    GameRegistry.registerBlock(lGlowstone, "lGlowstone");
-	    
-		GameRegistry.registerBlock(airCobblestone, "airCobblestone");
-		GameRegistry.registerBlock(airStone, "airStone");
-		GameRegistry.registerBlock(airGlowstone, "airGlowstone");
+	    //GameRegistry.registerBlock(lCobblestone, "lCobblestone");
+	    //GameRegistry.registerBlock(lStone, "lStone");
+	    //GameRegistry.registerBlock(lGlowstone, "lGlowstone");
+	       
+        GameRegistry.registerBlock(fakeCobblestone, "fakeCobblestone");
+        GameRegistry.registerBlock(fakeStone, "fakeStone");
+        GameRegistry.registerBlock(fakeGlowstone, "fakeGlowstone");
+        GameRegistry.registerBlock(fakeStoneBrick, "fakeStoneBrick");
+        GameRegistry.registerBlock(fakeBrokenStoneBrick, "fakeBrockenStoneBrick");
+        GameRegistry.registerBlock(fakeMossyStoneBrick, "fakeMossyStoneBrick");
+        GameRegistry.registerBlock(fakeFancyStoneBrick, "fakeFancyStoneBrick");
+        GameRegistry.registerBlock(fakeBrichPlanks, "fakeBrichPlanks");
+        GameRegistry.registerBlock(fakeJunglePlanks, "fakeJunglePlanks");
+        GameRegistry.registerBlock(fakeSprucePlanks, "fakeSprucePlanks");
+        GameRegistry.registerBlock(fakeOakPlanks, "fakeOakPlanks");
+        GameRegistry.registerBlock(fakeAcaciaPlanks, "fakeAcaciaPlanks");
+        GameRegistry.registerBlock(fakeDarkOakPlanks, "fakeDarkOakPlanks"); 
+        GameRegistry.registerBlock(fakeBrick, "fakeBrick");
+        GameRegistry.registerBlock(fakeGlass, "fakeGlass");
 		
 		GameRegistry.registerBlock(crystalLatern, "crystalLatern");
 		GameRegistry.registerBlock(plastic, "plastic");
